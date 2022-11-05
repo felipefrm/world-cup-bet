c8d209006151f213e9dded288fda44031299901b
Felipe Melo <felipefrmelo@hotmail.com>
2022-11-05 18:12:55 -0300

    add navigation

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,10 +1,13 @@
 import { Icon, VStack } from "native-base";
 import { Octicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 
 export function Polls() {
+  const { navigate } = useNavigation();
+
   return (
     <VStack flex="1" bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -14,10 +17,11 @@ export function Polls() {
         borderBottomColor="gray.600"
         borderBottomWidth="1"
       >
-       <Button
-         title="BUSCAR BOLÃO POR CÓDIGO"
-        leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
-       />
+        <Button
+          title="BUSCAR BOLÃO POR CÓDIGO"
+          leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+          onPress={() => navigate('find')}
+        />
       </VStack>
     </VStack>
   )
